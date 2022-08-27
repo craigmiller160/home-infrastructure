6cdd142b2bf93493367d1bb988b6e05a42472c5b
Craig Miller <craigmiller160@gmail.com>
2022-08-27 11:05:39 -0400

    Working on deployments

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,14 @@
 # TODO this will need to be configurable
 K8S_CONTEXT=microk8s-dev
 
-function ensure_chart_name {
+function validate_deployment_directory {
   if [ $# -lt 1 ]; then
-    echo "Must provide chart name"
+    echo "Must provide deployment directory"
+    exit 1
+  fi
+
+  if [ ! -d $1 ]; then
+    echo "Invalid deployment directory: $1"
     exit 1
   fi
 }
@@ -14,15 +19,14 @@ function deploy_chart {
   if [ -f ./$1/repo.sh ]; then
     sh ./$1/repo.sh
   fi
+  # TODO figure out how to derive chart name and --set-file from directory
   helm install \
     $1 \
-    # TODO figure out how to derive this from directory
     1password/connect \
     --kube-context=microk8s-dev \
     --values ./$1/values.yml \
-    # TODO figure out how to derive this from directory
     --set-file connect.credentials=./$1/1password_creds.json
 }
 
-ensure_chart_name $@
+validate_deployment_directory $@
 deploy_chart $@
