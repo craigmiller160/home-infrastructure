de4a6dedd58921876b84cf4ba751bf2e5629514f
Craig Miller <craigmiller160@gmail.com>
2022-08-27 11:16:17 -0400

    More script work

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,16 @@
 # Script Arguments
 # $1 = environment, $2 = command, $3 = deployment directory
 
-function validate_env {
+function ensure_repos_added {
+  helm repo add 1password https://1password.github.io/connect-helm-charts
+}
+
+function validate_arguments {
+  if [ $# -lt 3 ]; then
+    echo "Must provide environment, command, and deployment directory"
+    exit 1
+  fi
+
   case $1 in
     "dev"|"prod") return 0 ;;
     *)
@@ -11,17 +20,6 @@ function validate_env {
       exit 1
     ;;
   esac
-}
-
-function ensure_repos_added {
-  helm repo add 1password https://1password.github.io/connect-helm-charts
-}
-
-function validate_deployment_directory {
-  if [ $# -lt 1 ]; then
-    echo "Must provide deployment directory"
-    exit 1
-  fi
 
   if [ ! -d $1 ]; then
     echo "Invalid deployment directory: $1"
@@ -63,7 +61,6 @@ function run_chart_command {
   esac
 }
 
-validate_env $@
-validate_deployment_directory $@
+validate_arguments $@
 ensure_repos_added $@
 run_chart_command $@
