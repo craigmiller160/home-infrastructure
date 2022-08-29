e4fa842449d839d79dd851526056551873e85bf5
Craig Miller <craigmiller160@gmail.com>
2022-08-28 20:35:47 -0400

    got everything working for new 1password setup

--- 1password/settings.sh
+++ 1password/settings.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 chart_name=./1password
-arguments="--set-file connect.credentials=./$3/1password_creds.json --set operator.token.value=$(cat ./$3/1password_token.txt)"
+arguments="--set-file connect.connect.credentials=./$3/1password_creds.json --set connect.operator.token.value=$(cat ./$3/1password_token.txt)"
