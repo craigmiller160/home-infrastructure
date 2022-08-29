e48bdda9f623d3431e30f980d0db8e780cf47bf1
Craig Miller <craigmiller160@gmail.com>
2022-08-29 18:11:25 -0400

    Some refactoring of cert manager

--- helm_deployments/deploy.sh
+++ helm_deployments/deploy.sh
@@ -6,6 +6,7 @@
 function ensure_repos_added {
   helm repo add 1password https://1password.github.io/connect-helm-charts
   helm repo add bitnami https://charts.bitnami.com/bitnami
+  helm repo add jetstack https://charts.jetstack.io
 }
 
 function validate_arguments {
