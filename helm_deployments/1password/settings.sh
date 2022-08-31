c737f1713cbf018b418b7ccdfe0369a2e412ed53
Craig Miller <craigmiller160@gmail.com>
2022-08-31 16:54:18 -0400

    Updated settings files for change in deploy arguments

--- helm_deployments/1password/settings.sh
+++ helm_deployments/1password/settings.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-arguments="--set-file connect.connect.credentials=./$3/1password_creds.json --set connect.operator.token.value=$(cat ./$3/1password_token.txt)"
+arguments="--set-file connect.connect.credentials=./$2/1password_creds.json --set connect.operator.token.value=$(cat ./$2/1password_token.txt)"
