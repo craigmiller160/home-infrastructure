7d3bb1a6963610862ca268c83759d82a00c580e0
Craig Miller <craigmiller160@gmail.com>
2022-09-02 15:59:24 -0400

    Kinda sorta maybe making progress

--- terraform_config/nexus/main.tf
+++ terraform_config/nexus/main.tf
@@ -18,6 +18,7 @@ terraform {
 }
 
 provider "onepassword" {
+  # TODO need to support prod
   url = "https://dev-infra-craigmiller160.ddns.net/onepassword"
   token = var.onepassword_token
 }
@@ -27,9 +28,10 @@ data "onepassword_item" "nexus_admin" {
   uuid = "p7w642mhmlyjf7k2myeik2buey"
 }
 
-#provider "nexus" {
-#  insecure = true
-#  password = var.nexus_admin_password
-#  username = "admin"
-#  url = join("", ["https://", var.nexus_host])
-#}
+provider "nexus" {
+  insecure = true
+  password = data.onepassword_item.nexus_admin.password
+  username = "admin"
+  # TODO need to support prod
+  url = "https://dev-nexus-craigmiller160.ddns.net"
+}
