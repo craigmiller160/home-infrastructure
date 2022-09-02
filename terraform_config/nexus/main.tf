c4ca433b269878dff8b95939bd850670b053604d
Craig Miller <craigmiller160@gmail.com>
2022-09-02 15:39:54 -0400

    Setup 1password provider

--- terraform_config/nexus/main.tf
+++ terraform_config/nexus/main.tf
@@ -9,12 +9,21 @@ terraform {
       source  = "datadrivers/nexus"
       version = "1.21.0"
     }
+
+    onepassword = {
+      source = "1Password/onepassword"
+      version = "1.1.4"
+    }
   }
 }
 
-provider "nexus" {
-  insecure = true
-  password = var.nexus_admin_password
-  username = "admin"
-  url = join("", ["https://", var.nexus_host])
+provider "onepassword" {
+  url = "https://dev-infra-craigmiller160/onepassword"
 }
+
+#provider "nexus" {
+#  insecure = true
+#  password = var.nexus_admin_password
+#  username = "admin"
+#  url = join("", ["https://", var.nexus_host])
+#}
