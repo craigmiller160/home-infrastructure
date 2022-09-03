90bdf20d630e436cce4ce33bb379f8e67c4a4cf1
Craig Miller <craigmiller160@gmail.com>
2022-09-03 14:12:40 -0400

    Getting import script working

--- terraform_config/nexus/20220903_import.sh
+++ terraform_config/nexus/20220903_import.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 # Imports all data necessary for the upcoming migration
+# $1 = env
 
+import_script_dir=$(dirname "${BASH_SOURCE[0]}")
+
+IFS=$'\n'
+resources=(
+  "nexus_blobstore_file.npm_private npm-private"
+)
+
+for r in ${resources[@]}; do
+  $import_script_dir/run.sh $1 import $r
+done
