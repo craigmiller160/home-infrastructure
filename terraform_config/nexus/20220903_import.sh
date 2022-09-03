decf2c44c8525e4bf0cf70a27eb6b3635889353c
Craig Miller <craigmiller160@gmail.com>
2022-09-03 14:13:49 -0400

    More script improvements

--- terraform_config/nexus/20220903_import.sh
+++ terraform_config/nexus/20220903_import.sh
@@ -10,6 +10,18 @@ resources=(
   "nexus_blobstore_file.npm_private npm-private"
 )
 
-for r in ${resources[@]}; do
-  $import_script_dir/run.sh $1 import $r
-done
+function validate_arguments {
+  if [ $# -lt 1 ]; then
+    echo "Must specify environment"
+    exit 1
+  fi
+}
+
+function import {
+  for r in ${resources[@]}; do
+    $import_script_dir/run.sh $1 import $r
+  done
+}
+
+validate_arguments $@
+import $@
