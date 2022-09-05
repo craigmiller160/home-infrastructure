f5128c73208c739148b12b08f649f0af572c1ac4
Craig Miller <craigmiller160@gmail.com>
2022-09-05 18:00:14 -0400

    Fixed blob configuration

--- terraform_config/nexus/blobs.tf
+++ terraform_config/nexus/blobs.tf
@@ -1,34 +1,37 @@
+# IMPORTANT - The paths must be relative paths because that is how they were first written, and making them absolute paths breaks
+# the existing data configuration in Nexus
+
 resource "nexus_blobstore_file" "npm_private" {
   name = "npm-private"
-  path = "/nexus-data/npm-private"
+  path = "npm-private"
 }
 
 resource "nexus_blobstore_file" "npm_proxy" {
   name = "npm-proxy"
-  path = "/nexus-data/npm-proxy"
+  path = "npm-proxy"
 }
 
 resource "nexus_blobstore_file" "npm_group" {
   name = "npm-group"
-  path = "/nexus-data/npm-group"
+  path = "npm-group"
 }
 
 resource "nexus_blobstore_file" "docker_private" {
   name = "docker-private"
-  path = "/nexus-data/docker-private"
+  path = "docker-private"
 }
 
 resource "nexus_blobstore_file" "docker_proxy" {
   name = "docker-proxy"
-  path = "/nexus-data/docker-proxy"
+  path = "docker-proxy"
 }
 
 resource "nexus_blobstore_file" "docker_group" {
   name = "docker-group"
-  path = "/nexus-data/docker-group"
+  path = "docker-group"
 }
 
 resource "nexus_blobstore_file" "helm_private" {
   name = "helm-private"
-  path = "/nexus-data/helm-private"
+  path = "helm-private"
 }
