92560a0152f0c963da58f2bd221cfbc8b686fc81
Craig Miller <craigmiller160@gmail.com>
2022-09-05 16:27:57 -0400

    Updating script to use new variables

--- helm_deployments/ingress/run.sh
+++ helm_deployments/ingress/run.sh
@@ -9,4 +9,10 @@ else
   arguments=""
 fi
 
-run ingress $@
+function run_for_namespace {
+  OVERRIDE_NAMESPACE=$1
+  OVERRIDE_VALUES_FILE=$2
+  run ingress ${@:3}
+}
+
+run_for_namespace infra-dev values.infra.yml $@
