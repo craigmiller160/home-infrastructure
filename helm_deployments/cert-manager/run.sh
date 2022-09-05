bc6488c012e2d427485c6a38b5e72ed58ec7e25b
Craig Miller <craigmiller160@gmail.com>
2022-09-05 10:52:15 -0400

    Renamed helm deploy script

--- helm_deployments/cert-manager/run.sh
+++ helm_deployments/cert-manager/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 run_script_dir=$(dirname "${BASH_SOURCE[0]}")
-source "$run_script_dir/../../scripts/helm.sh"
+source "$run_script_dir/../../scripts/helm_deploy.sh"
 
 arguments="--set cert-manager.installCRDs=true"
 
--- helm_deployments/postgres/run.sh
+++ helm_deployments/postgres/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 run_script_dir=$(dirname "${BASH_SOURCE[0]}")
-source "$run_script_dir/../../scripts/helm.sh"
+source "$run_script_dir/../../scripts/helm_deploy.sh"
 
 run postgres $@
