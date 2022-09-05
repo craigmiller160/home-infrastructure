9df4f0f1676f7050bafc7c43fcf0def6866bd461
Craig Miller <craigmiller160@gmail.com>
2022-09-05 14:42:02 -0400

    Added more namespace variables

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -2,6 +2,8 @@
 
 k8s_context=microk8s-prod
 dev_infra_namespace=infra-dev
+dev_apps_namespace=apps-dev
 prod_infra_namespace=infra-prod
+prod_apps_namespace=apps-prod
 dev_nexus=dev-nexus-craigmiller160.ddns.net
 prod_nexus=nexus-craigmiller160.ddns.net
