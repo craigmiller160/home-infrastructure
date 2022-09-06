f0e980555502e86672c93693a6b3f5213e3f365a
Craig Miller <craigmiller160@gmail.com>
2022-09-06 06:38:22 -0400

    Slight tweak to script

--- helm_deployments/1password/run.sh
+++ helm_deployments/1password/run.sh
@@ -4,6 +4,6 @@ run_script_dir=$(dirname "${BASH_SOURCE[0]}")
 source "$run_script_dir/../../scripts/helm_deploy.sh"
 source "$run_script_dir/../../.secrets"
 
-arguments="--set connect.operator.token.value=$onepassword_token --set connect.connect.credentials_base64=$onepassword_creds"
+arguments="--set connect.operator.token.value=$onepassword_token --set connect.connect.credentials_base64=$onepassword_creds --set environment=$1"
 
 run 1password $@
