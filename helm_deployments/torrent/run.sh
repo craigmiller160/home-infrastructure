cd7a43a50a4c0b97366db20f7050339007442162
Craig Miller <craigmiller160@gmail.com>
2022-09-12 16:18:14 -0400

    Working on final prep on the torrent app

--- helm_deployments/torrent/run.sh
+++ helm_deployments/torrent/run.sh
@@ -4,5 +4,6 @@ run_script_dir=$(dirname "${BASH_SOURCE[0]}")
 source "$run_script_dir/../../scripts/helm_deploy.sh"
 
 arguments="--set environment=$1"
+OVERRIDE_NAMESPACE="apps-$1"
 
 run torrent $@
