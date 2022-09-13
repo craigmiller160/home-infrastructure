11f3e31acdb31eecbf4090b120aa71ee30cadbfa
Craig Miller <craigmiller160@gmail.com>
2022-09-13 07:19:21 -0400

    Added repo

--- helm_repos.sh
+++ helm_repos.sh
@@ -6,6 +6,7 @@ function ensure_repos_added {
   helm repo add jetstack https://charts.jetstack.io
 #  helm repo add dev_nexus https://dev-nexus-craigmiller160.ddns.net/repository/helm-private
   helm repo add prod_nexus https://nexus-craigmiller160.ddns.net/repository/helm-private
+  helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
 }
 
 ensure_repos_added
