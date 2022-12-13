5efdbc6d81b41a560b75e873c125a63e11df953c
Craig Miller <craigmiller160@gmail.com>
2022-12-13 16:55:23 -0500

    Working on run script

--- helm_deployments/prometheus/run.sh
+++ helm_deployments/prometheus/run.sh
@@ -4,12 +4,6 @@ run_script_dir=$(dirname "${BASH_SOURCE[0]}")
 source "$run_script_dir/../../scripts/helm_deploy.sh"
 source "$run_script_dir/../../.secrets"
 
-function run_for_namespace {
-  OVERRIDE_NAMESPACE=$1
-  OVERRIDE_VALUES_FILE=$2
-  run ingress ${@:3}
-}
-
 OVERRIDE_NAMESPACE="prometheus"
 arguments="--set kube-prometheus-stack.grafana.adminPassword=$grafana_admin_password"
-run
+run prometheus "x" $1
