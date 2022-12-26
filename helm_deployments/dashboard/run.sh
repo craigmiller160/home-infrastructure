369a58a56fdb549e7b3c0d016bee43ca679f3c2b
Craig Miller <craigmiller160@gmail.com>
2022-12-25 20:27:07 -0500

    Fixed script

--- helm_deployments/dashboard/run.sh
+++ helm_deployments/dashboard/run.sh
@@ -3,6 +3,4 @@
 run_script_dir=$(dirname "${BASH_SOURCE[0]}")
 source "$run_script_dir/../../scripts/helm_deploy.sh"
 
-arguments="--set cert-manager.installCRDs=true"
-
-run cert-manager $@
+run dashboard $@
